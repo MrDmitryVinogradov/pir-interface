b282528ee000c3e7cb078dec1f46db55dabd122c
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2022-07-12 13:45:41 +0300

    basic interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="search" placeholder="Поиск"></input>
+      <div className="page-wrapper">
+        <nav className="main-menu">
+          <ul className="main-menu-list">
+            <li className="main-menu-item"> <a href="#" className="main-menu-item-link"> Мои дела </a> </li>
+            <li className="main-menu-item"> <a href="#" className="main-menu-item-link"> Мои задачи </a> </li>
+            <li className="main-menu-item"> <a href="#" className="main-menu-item-link"> Мой календарь </a> </li>
+            <li className="main-menu-item"> <a href="#" className="main-menu-item-link"> Оповещения </a> </li>
+            <li className="main-menu-item"> <a href="#" className="main-menu-item-link"> Мой отдел </a> </li>
+            <li className="main-menu-item"> <a href="#" className="main-menu-item-link"> Справка </a> </li>
+          </ul>
+        </nav>
+        <main className="workzone">
+          <nav className="workzone-menu">
+            <div className="workzone-menu-wrapper">
+            <ul className="workzone-menu-list">
+              <li className="workzone-menu-item"> <a className="workzone-menu-item-link" href="#"> В подготовке </a></li>
+              <li className="workzone-menu-item"> <a className="workzone-menu-item-link" href="#"> В судах </a></li>
+              <li className="workzone-menu-item"> <a className="workzone-menu-item-link" href="#"> В ожидании </a></li>
+              <li className="workzone-menu-item"> <a className="workzone-menu-item-link" href="#"> Архив </a></li>
+              </ul>
+            </div>
+            <ul className="aside-menu">
+              <li className="aside-menu-item"> <a href="#" className="aside-menu-item-link"> Общее производство </a> </li>
+              <li className="aside-menu-item"> <a href="#" className="aside-menu-item-link"> Упрощенное производство </a> </li>
+              <li className="aside-menu-item"> <a href="#" className="aside-menu-item-link"> Приказное производство </a> </li>
+              <li className="aside-menu-item"> <a href="#" className="aside-menu-item-link"> Обжалование </a> </li>
+            </ul>
+          </nav>
+          <div className="workzone-list">
+            Это рабочая зона, в которой должен отображаться перечень дел / перечень задач / календарь / список оповещений / список работников в зависимости от выбранного пункта в главном меню
+          </div>
+        </main>
+        <aside className="case-info">
+        Здесь должна отображаться краткая информация о выбранном пункте в рабочей зоне
+        </aside>
+      </div>
     </div>
   );
 }
